3695e161401f43427e80e132d5d89bf99c2acb18
zhaowenyi94 <zhaowenyi94@gmail.com>
2017-09-24 01:00:50 -0700

    Fake Fountain like feed

--- src/IdeaPanel.js
+++ src/IdeaPanel.js
@@ -8,9 +8,14 @@ class IdeaPanel extends Component {
     render() {
         return (
             <div className="IdeaPanel">
-                    <IdeaItem author="1" title="Thinking about building a web app for dog owner to monitor their dog's health"/>
-                    <IdeaItem author="2" title="What about developing an application for making friends on-campus"/>
-                    <IdeaItem author="3" title="A web"/>
+                <div className="idea-col">
+                    <IdeaItem author="1" title="Building a monitor web app for dog ownerDeveloping an App for on-campus social event"/>
+                    <IdeaItem author="2" title="Developing an App for on-campus social event"/>
+                </div>
+                <div className="idea-col">
+                    <IdeaItem author="3" title="Personal cooking planer, making our life easier "/>
+                    <IdeaItem author="4" title="Attending RoboCap, want to join?"/>
+                </div>
             </div>
         )
     }
